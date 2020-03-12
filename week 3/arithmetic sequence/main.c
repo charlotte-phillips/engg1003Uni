7d72f63d7ed4b2b7e57385a99313e55d5fe0b37b
Charlotte Phillips <c3356044@uon.edu.au>
2020-03-12 14:11:01 +1100

    Signed-off-by: Charlotte Phillips <c3356044@uon.edu.au> arithmetic sequence bullsht

--- week 3/arithmetic sequence/main.c
+++ week 3/arithmetic sequence/main.c
@@ -2,7 +2,28 @@
 #include <stdlib.h>
 
 int main()
-{
-    printf("Hello world!\n");
-    return 0;
+{   float xnm1 = 2, xn = 4, k = 2, sum = 0;
+    int count = 1;
+    printf("0 %f\n", xnm1);
+    if (k >-1 && k < 1) {
+        while (count < 100 && xn > 0.0001){
+        xn = xnm1 * k;
+        printf("%d %f\n", count, xn);
+        k = xn / xnm1;
+        xnm1 = xn;
+        count++;
+        }
+    }
+    else if (k <=1 || k >=1) {
+        while (count < 10 ){
+        xn = xnm1 * k;
+        printf("%d %f\n", count, xn);
+        k = xn / xnm1;
+        xnm1 = xn;
+        count++;
+        }
+    }
+ return 0;
+
+
 }
